f6f6025f4368a8dd8fa46fcb02e2b193fa4c1fdb
j0thu <jonathanben215@gmail.com>
2020-07-08 18:59:50 +0530

    First Commit

---
+++ script.js
@@ -0,0 +1,21 @@
+replaceText(document.body);
+
+function replaceText(element){
+    if(element.hasChildNodes()){
+        element.childNodes.forEach(replaceText);
+    }
+    else if(element.nodeType === Text.TEXT_NODE){
+        if(element.textContent.match(/coronavirus/gi)){
+            // element.parentElement.style.color = 'black'
+            // element.parentElement.style.backgroundColor = 'black'
+            const newElement = document.createElement('span');
+            newElement.innerHTML = element.textContent.replace(/(coronavirus)/gi, '<span class="rainbow">$1</span>')
+            element.replaceWith(newElement);
+        
+        //newElement.innerHTML = element.textContent.replace(/(coronavirus)/gi, '<span style = "background-color: black; color: black;">$1</span>')
+
+    }
+         // element.parentElement.remove(); //To remove the entire para containing coronavirus
+    }
+}
+
